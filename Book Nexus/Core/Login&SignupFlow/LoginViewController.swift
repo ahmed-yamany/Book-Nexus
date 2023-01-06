72c88fa246046e9b2242d243b2f47de5ba5ff5dd
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2023-01-06 12:12:17 +0200

    UITextField+setPaddingPoints

--- Book Nexus/Core/Login&SignupFlow/LoginViewController.swift
+++ Book Nexus/Core/Login&SignupFlow/LoginViewController.swift
@@ -8,74 +8,38 @@
 import UIKit
 import SwiftUI
 
-class LoginViewController: UIViewController {
-    // MARK: - View controller subViews
-     private let imageView: UIImageView = {
-         let image = UIImageView(image: ImageManager.LoginSignUpFlow)
-         image.contentMode = .scaleToFill
-         return image
+class LoginViewController: LoginSignUpFlowViewController {
+    // MARK: - View controller SubViews
+    let emailTextField = FormTextField("Email")
+    let continueButton = AccentGreenButton("Continue")
+    let forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Forgot password?", for: .normal)
+        button.setTitleColor(ColorsManager.accentGreen, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+
+        return button
     }()
-    
-    let blueredView: BluredView = BluredView()
-    
-    let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        
-        
-        return stackView
-    }()
-    
-    
-    
+
     
     // MARK: - View controller lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.text = "Log in"
         
-        view.addSubview(imageView)
-        imageView.fillSuperviewConstraints()
         
-        view.addSubview(blueredView)
-        blueredView.updateLayoute()
         
-        ///
-        
-        blueredView.addSubview(stackView)
-        stackView.fillSuperviewConstraints(UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20))
-        
-//
-        stackView.arrangedSubviews.forEach { view in
-            view.heightConstraints(48)
-            view.layer.cornerRadius = 8
-        }
+        stackView.addArrangedSubview(emailTextField)
+        stackView.addArrangedSubview(continueButton)
+        stackView.addArrangedSubview(forgotPasswordButton)
+        stackView.updateArrangedSubviewsLayout()
 
-//        let button = UIButton()
-//        button.setTitle("Continue", for: .normal)
-//        button.setTitleColor(ColorsManager.accentBlack, for: .normal)
-//        button.backgroundColor = ColorsManager.accentGreen
-//        button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .bold)
-//
-//
-//        let button2 = UIButton()
-//        button2.setTitle("Ahmed yamany", for: .normal)
-//        button2.tintColor = .white
-//        button2.backgroundColor = .blue
-//
-//
-//        stackView.addArrangedSubview(button)
-//        stackView.addArrangedSubview(button2)
-//
-        
-        
     }
-    
-    
-    
-
 
 }
+
+
+
 
 //extension
 struct LoginView: UIViewControllerRepresentable {
---
+++ Book Nexus/Core/Login&SignupFlow/SubClasses/FormTextField.swift
@@ -0,0 +1,22 @@
+//
+//  FormTextField.swift
+//  Book Nexus
+//
+//  Created by Ahmed Yamany on 06/01/2023.
+//
+
+import UIKit
+
+class FormTextField: UITextField{
+    init(_ placeholder: String){
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        self.backgroundColor = .white
+        self.setLeftPaddingPoints(20)
+    
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
